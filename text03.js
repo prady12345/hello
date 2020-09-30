1e6d47ab2d097e1f8ca8ed0be1acb1ed23acd1e8
Piyush Pandey <pandey.piyus@gmail.com>
2020-10-01 00:04:48 +0530

    Update text03.js

--- text03.js
+++ text03.js
@@ -3,3 +3,13 @@ function greet(name) {
 }
 
 greet('piyush');
+
+function greet(name) {
+    console.log(`Hello ${name} how are you today?`);
+}
+
+greet('piyush');
+function greet(name) {
+    console.log(`Hello ${name} how are you today?`);
+}
+
